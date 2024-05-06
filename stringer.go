db1b2c0460e281482f18752babe991cbebfa11b4
Marijn Koesen <github@koesen.nl>
2024-05-06 14:24:19 +0200

    Add BSON support

--- stringer.go
+++ stringer.go
@@ -50,6 +50,7 @@ var (
 	yaml            = flag.Bool("yaml", false, "if true, yaml marshaling methods will be generated. Default: false")
 	text            = flag.Bool("text", false, "if true, text marshaling methods will be generated. Default: false")
 	gqlgen          = flag.Bool("gqlgen", false, "if true, GraphQL marshaling methods for gqlgen will be generated. Default: false")
+	bson            = flag.Bool("bson", false, "if true, bson marshaling methods will be generated. Default: false")
 	altValuesFunc   = flag.Bool("values", false, "if true, alternative string values method will be generated. Default: false")
 	output          = flag.String("output", "", "output file name; default srcdir/<type>_string.go")
 	transformMethod = flag.String("transform", "noop", "enum item name transformation method. Default: noop")
@@ -127,6 +128,10 @@ func main() {
 	if *json {
 		g.Printf("\t\"encoding/json\"\n")
 	}
+	if *bson {
+		g.Printf("\t\"go.mongodb.org/mongo-driver/bson/bsontype\"\n")
+		g.Printf("\t\"go.mongodb.org/mongo-driver/x/bsonx/bsoncore\"\n")
+	}
 	if *gqlgen {
 		g.Printf("\t\"io\"\n")
 		g.Printf("\t\"strconv\"\n")
@@ -135,7 +140,7 @@ func main() {
 
 	// Run generate for each type.
 	for _, typeName := range typs {
-		g.generate(typeName, *json, *yaml, *sql, *text, *gqlgen, *transformMethod, *trimPrefix, *addPrefix, *linecomment, *altValuesFunc)
+		g.generate(typeName, *json, *yaml, *sql, *text, *bson, *gqlgen, *transformMethod, *trimPrefix, *addPrefix, *linecomment, *altValuesFunc)
 	}
 
 	// Format the output.
@@ -414,7 +419,7 @@ func (g *Generator) prefixValueNames(values []Value, prefix string) {
 
 // generate produces the String method for the named type.
 func (g *Generator) generate(typeName string,
-	includeJSON, includeYAML, includeSQL, includeText, includeGQLGen bool,
+	includeJSON, includeYAML, includeSQL, includeText, includeBSON, includeGQLGen bool,
 	transformMethod string, trimPrefix string, addPrefix string, lineComment bool, includeValuesMethod bool) {
 	values := make([]Value, 0, 100)
 	for _, file := range g.pkg.files {
@@ -474,6 +479,9 @@ func (g *Generator) generate(typeName string,
 	}
 	if includeText {
 		g.buildTextMethods(runs, typeName, runsThreshold)
+	}
+	if includeBSON {
+		g.buildBSONMethods(runs, typeName, runsThreshold)
 	}
 	if includeYAML {
 		g.buildYAMLMethods(runs, typeName, runsThreshold)
